2ee373523a200fef05323c072e5e823f88a47064
Italoac <italoac@gmail.com>
2023-02-17 18:48:05 -0300

    testando alguns tipo de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,35 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Testando short para int e int para short e usando cast
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Testando Long
+        long totalPopulacao = 801706837400L;
+        System.out.printf("Mostre o total da população mundial: %d", (totalPopulacao));
+
+        
+        //Testando String
+        String nome = "Ítalo";
+        String sobrenome = "Marshal";
+        System.out.printf("\nSeu nome completo: " + nome + " " + sobrenome);
+
+        //Testando char
+        char opcaoSim = 'S';
+        char opcaoNao = 'N';
+
+        System.out.printf("\nMiguel é mulher? %c", opcaoNao);
+        System.out.println("\nAmanda é mulher? " + opcaoSim);
+
+
+        //Exemplo de variável constante
+        int numero = 5;
+        numero = 10;
+
+        System.out.printf("\nNúmeros inteiros %d\n", numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
